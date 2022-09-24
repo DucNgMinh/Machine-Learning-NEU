a2409619db1f5f1c197c3e971fc025936a4d1617
DucNgMinh <minhduc02d@gmail.com>
2022-09-24 21:18:53 +0700

    add Linear Regression file

---
+++ Linear Regression/Linear_Regression.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+class LinearRegression():
+    def __init__(self):
+        self.theta = None
+        self.alpha = 0.01 # step size
+        self.elpison = 0.001 # error after comma
+
+    def fit(self, x, y):
+        # create X matrix
+        one_col = np.ones((x.shape[0],1)) # create columns for bias feature ( m * 1 matrix with 1)
+        X = np.concatenate((one_col,x), axis= 1)
+
+        # calculate thetea
+        self.theta = np.linalg.pinv(X.T @ X) @ X.T @ y
+
+    def predict(self, x):
+        return np.sum(np.dot(x, self.theta[1:]) + self.theta[0])
